4a0206252a279550d25fbfb916d4d73d3ee1f16f
Juan Carlos Pablo <juan.pablo@palaceresorts.com>
2024-05-05 15:14:24 -0600

    contact form and colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,23 @@ module.exports = {
       colors: {
         green: "#28CB8B",
         silver: "#F5F7FA",
+        neutral: {
+          black: "#263238",
+          silver: "#F5F7FA",
+          "grey-blue": "#ABBED1",
+          grey: "#89939E",
+          "d-grey": "#4D4D4D",
+        },
+        primary: {
+          greee: "#28CB8B",
+          black: "#263238",
+          blue: "#2194f3",
+        },
+        action: {
+          warning: "#FBC02D",
+          error: "#E53835",
+          success: "#2E7D31",
+        }
       },
     },
   },
