7bb9557d9fb766d91e0a4ffcc6d3fb0aa88ac3de
Juan Carlos Pablo <juan.pablo@palaceresorts.com>
2024-05-04 19:23:54 -0600

    initial layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontFamily: {
+      'sans': 'Open Sans',
+      'serif': "Montserrat",
+    },
+    colors: {
+      green: "#28CB8B",
+    },
     extend: {},
   },
   plugins: [],
